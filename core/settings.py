2f15d9d78984883f44bca9e8c58c377bdcc2ba29
GabrielMD08 <bielmdomingos08@gmail.com>
2025-10-27 11:53:00 -0300

    add allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = Path('./lista-de-contatos/django_stuff.json')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
