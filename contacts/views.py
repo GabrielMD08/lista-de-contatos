0164db564b825876b0479d66812bfb33c8decb46
GabrielMD08 <bielmdomingos08@gmail.com>
2025-11-10 15:08:45 -0300

    create index and new_contact views. and craete contacs urls

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.urls import reverse
+from . import forms
 
-# Create your views here.
+def index(request):
+    return reverse('index')
+
+def new_contact(request):
+    
+    if request.method == 'POST':
+        
+        form = forms.ContactForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return reverse('index')
+    
+    else :
+        form = forms.ContactForm()
+    
+    return render(request, 'templates/new_contact', {'form': form})
