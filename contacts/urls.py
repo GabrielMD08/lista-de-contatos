2333b199a76fe0bc4abb44f95979c5a396a31729
GabrielMD08 <bielmdomingos08@gmail.com>
2025-11-16 03:13:00 -0300

    change the index url to core

--- contacts/urls.py
+++ contacts/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.index, name='index'),
     path('novo-contato/', views.new_contact, name='novo contato'),
 ]
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contacts/', include('contacts.urls'), name='contacts')
+    path('', views.index, name='index'),
+    path('contacts/', include('contacts.urls'), name='contacts'),
 
 ]
