044462d1574e8e0ea5b1a7fb97da9507b251c573
GabrielMD08 <bielmdomingos08@gmail.com>
2026-06-23 12:51:37 -0300

    adjust: add blank=true to fix form problem

--- contacts/models.py
+++ contacts/models.py
@@ -3,8 +3,8 @@ from django.core.validators import RegexValidator
 
 class Contact(models.Model):
     first_name = models.CharField('Nome', max_length=30)
-    last_name = models.CharField('Sobrenome', max_length=60, null=True)
-    nick_name = models.CharField('Apelido', max_length=30, null=True)
+    last_name = models.CharField('Sobrenome', max_length=60, null=True, blank=True )
+    nick_name = models.CharField('Apelido', max_length=30, null=True, blank=True)
 
     phone_validator = RegexValidator(
             regex= r'^\+55\d{10,11}',
@@ -12,13 +12,16 @@ class Contact(models.Model):
             )
 
     phone_number = models.CharField('Numero de telefone', max_length=14, validators=[phone_validator], unique=True)
-    email = models.EmailField('Email', null= True)
+    email = models.EmailField('Email', null=True, blank=True)
 
     def __str__(self):
         return f'{self.first_name}'
     
     def show_full_name(self):
-        return f'{self.first_name} {self.last_name}'
-    
+        if self.last_name == None:
+            return f'{self.first_name}'
+        else:
+            return f'{self.first_name} {self.last_name}'
+        
     def show_nick_name(self):
         return f'{self.nick_name}'
